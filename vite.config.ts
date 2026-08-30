e6ee8f13e41ef78fc9a8c57d691621ee82da3cb5
rdcamaleao-tech <rdcamaleao@gmail.com>
2026-08-30 11:49:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/site-radio-camaleao/',
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-});
+  plugins: [react()],
+  base: '/',
+})
