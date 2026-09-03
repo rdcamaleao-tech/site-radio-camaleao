57937d9569998bcef6702e49cf76061bef3b358d
rdcamaleao-tech <rdcamaleao@gmail.com>
2026-09-02 21:05:37 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
